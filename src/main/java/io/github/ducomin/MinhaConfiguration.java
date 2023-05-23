810707be20ef679fcca1a984d3435696d5f07b24
Eduardo Comin <eduardo.comin@capgemini.com>
2023-05-23 16:31:25 -0300

    Aula 11 - Configuração externalizada - Application.properties.

--- src/main/java/io/github/ducomin/MinhaConfiguration.java
+++ src/main/java/io/github/ducomin/MinhaConfiguration.java
@@ -1,18 +1,7 @@
 package io.github.ducomin;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
-
-	@Bean(name = "applicationName")
-	public String applcationName() {
-		return "Sistema de vendas";
-	}
-
-	@Bean(name = "outraConfiguracao")
-	public String outraConfiguracao(){
-		return "Sistema de Vendas";
-	}
 }
