17a12b61d8b548aa5382bede318b19ac8eba88fd
Eduardo Comin <eduardo.comin@capgemini.com>
2023-05-23 16:19:17 -0300

    Aula 10 - Injeção de dependência.

---
+++ src/main/java/io/github/ducomin/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package io.github.ducomin.repository;
+
+import org.springframework.stereotype.Repository;
+
+import io.github.ducomin.model.Cliente;
+
+@Repository
+public class ClientesRepository {
+	public void persistir(Cliente cliente) {
+		//acessa a base e salvar o cliente
+	}
+}
