495620fe6513b4b1643f72ffa8e2f61c1aa7e1a6
Eduardo Comin <eduardo.comin@capgemini.com>
2023-05-25 15:33:39 -0300

    Aula 39 - Requisição POST e Resquest Body.

--- src/main/java/io/github/ducomin/VendasApplication.java
+++ src/main/java/io/github/ducomin/VendasApplication.java
@@ -1,24 +1,10 @@
 package io.github.ducomin;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.github.ducomin.domain.entity.Cliente;
-import io.github.ducomin.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
-
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
-		return args -> {
-			Cliente c = new Cliente(null, "Fulano");
-			clientes.save(c);
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/ducomin/rest/controller/ClienteController.java
+++ src/main/java/io/github/ducomin/rest/controller/ClienteController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,5 +41,11 @@ public class ClienteController {
 		return ResponseEntity.notFound().build();
 	}
 
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity save( @RequestBody Cliente cliente ){
+		Cliente clienteSalvo = clientes.save(cliente);
+		return ResponseEntity.ok(clienteSalvo);
+	}
 
 }
