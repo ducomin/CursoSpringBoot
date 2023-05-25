813c455631a733bd1f6effea0eb8ea0cc8a230f3
Eduardo Comin <eduardo.comin@capgemini.com>
2023-05-25 14:41:35 -0300

    Aula 38 - Requisição GET com parâmetros e Response Entity.

--- src/main/java/io/github/ducomin/domain/entity/Cliente.java
+++ src/main/java/io/github/ducomin/domain/entity/Cliente.java
@@ -1,10 +1,14 @@
 package io.github.ducomin.domain.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +22,9 @@ public class Cliente {
 
 	@Column(name = "nome", length = 100)
 	private String nome;
+
+	@OneToMany( mappedBy = "cliente" , fetch = FetchType.LAZY )
+	private Set<Pedido> pedidos;
 
 	public Cliente() {
 	}
@@ -45,6 +52,14 @@ public class Cliente {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Set<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(final Set<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
--- src/main/java/io/github/ducomin/rest/controller/ClienteController.java
+++ src/main/java/io/github/ducomin/rest/controller/ClienteController.java
@@ -1,19 +1,42 @@
 package io.github.ducomin.rest.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.github.ducomin.domain.entity.Cliente;
+import io.github.ducomin.domain.repository.Clientes;
+
 @Controller
 @RequestMapping(value = "/api/clientes")
 public class ClienteController {
+
+	@Autowired
+	private Clientes clientes;
 
 	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
 	@ResponseBody
 	public String helloCliente(@PathVariable("nome") String nomeCliente) {
 		return String.format("Hello %s", nomeCliente);
+	}
+
+	@GetMapping("/{id}")
+	@ResponseBody
+	public ResponseEntity getClienteById( @PathVariable Integer id ){
+		Optional<Cliente> cliente = clientes.findById(id);
+
+		if(cliente.isPresent()){
+			return ResponseEntity.ok( cliente.get() );
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 
 
