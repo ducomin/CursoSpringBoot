1d2165ce32a25089870c3336d4d1c4cb2326a8c1
Eduardo Comin <eduardo.comin@capgemini.com>
2023-05-30 11:28:51 -0300

    Aula 49 e 50 utilização do Lombok e criaçôes de DTOs.

--- src/main/java/io/github/ducomin/domain/entity/Cliente.java
+++ src/main/java/io/github/ducomin/domain/entity/Cliente.java
@@ -13,6 +13,13 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table( name = "cliente" )
 public class Cliente {
@@ -31,56 +38,4 @@ public class Cliente {
 	@JsonIgnore
 	@OneToMany( mappedBy = "cliente" , fetch = FetchType.LAZY )
 	private Set<Pedido> pedidos;
-
-	public Cliente() {
-	}
-
-	public Cliente(Integer id, String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
-
-	public Cliente(String nome) {
-		this.nome = nome;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(final String cpf) {
-		this.cpf = cpf;
-	}
-
-	public Set<Pedido> getPedidos() {
-		return pedidos;
-	}
-
-	public void setPedidos(final Set<Pedido> pedidos) {
-		this.pedidos = pedidos;
-	}
-
-	@Override
-	public String toString() {
-		return "Cliente{" +
-				"id=" + id +
-				", nome='" + nome + '\'' +
-				'}';
-	}
 }
