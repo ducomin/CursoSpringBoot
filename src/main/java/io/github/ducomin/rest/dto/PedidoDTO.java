691c5e63fcb3b8601d97c0f7e8a43ddfceff33fd
Eduardo Comin <eduardo.comin@capgemini.com>
2023-08-25 15:00:18 -0300

    Aula 63 - Validando a entidade Pedido.

--- src/main/java/io/github/ducomin/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/ducomin/rest/dto/PedidoDTO.java
@@ -3,6 +3,8 @@ package io.github.ducomin.rest.dto;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PedidoDTO {
+	@NotNull(message = "Informe o código do cliente.")
 	private Integer cliente;
+	@NotNull(message = "Campo Total do pedido é obrigatório.")
 	private BigDecimal total;
 	private List<ItemPedidoDTO> items;
 }
