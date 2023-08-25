bff2a7b983582b01bafece924b852fc6978c2760
Eduardo Comin <eduardo.comin@capgemini.com>
2023-08-25 14:51:25 -0300

    Aula 62 - Validando Produto.

--- src/main/java/io/github/ducomin/rest/controller/ProdutoController.java
+++ src/main/java/io/github/ducomin/rest/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package io.github.ducomin.rest.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -38,7 +40,7 @@ public class ProdutoController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Produto save(@RequestBody Produto produto) {
+	public Produto save(@RequestBody @Valid Produto produto) {
 		return repository.save(produto);
 	}
 
@@ -58,7 +60,7 @@ public class ProdutoController {
 	@PutMapping("{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void update(@PathVariable Integer id,
-			@RequestBody Produto produto) {
+			@RequestBody @Valid Produto produto) {
 		repository
 				.findById(id)
 				.map(produtoExistente -> {
